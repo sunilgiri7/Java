18f14bbdffbc38fd3e11c07c17658be54bcc4df9
user name <user email>
2022-12-03 03:43:54 -0800

    new

---
+++ Pattern_problems.java/pattern.java
@@ -0,0 +1,71 @@
+public class pattern{
+    public static void main(String[] args) {
+    pattern_6(4);
+    }
+
+    static void pattern_1(int n) {
+    for(int rows=1; rows<=n; rows++){
+        for(int cols=1; cols<=rows; cols++){
+            System.out.print("* ");
+        }
+        System.out.println("");
+    }
+}
+
+    static void pattern_2(int n) {
+    for(int rows=1; rows<=n; rows++){
+        for(int cols=1; cols<=n; cols++){
+            System.out.print("* ");
+        }
+        System.out.println("");
+    }
+}
+
+
+    static void pattern_3(int n) {
+    for(int rows=1; rows<=n; rows++){
+        for(int cols=1; cols<=n-rows+1; cols++){
+            System.out.print("* ");
+        }
+        System.out.println("");
+    }
+}
+
+    static void pattern_4(int n) {
+    for(int rows=1; rows<=n; rows++){
+        for(int cols=1; cols<=rows; cols++){
+            System.out.print(cols);
+        }
+        System.out.println("");
+    }
+}
+
+    static void pattern_5(int n) {
+    for(int rows=0; rows<=2*n; rows++){
+        int totalcolsinrow = rows>n ? 2*n-rows:rows;
+    	        for(int cols=0; cols<=totalcolsinrow; cols++){
+            System.out.print("* ");
+	        }
+	        System.out.println("");
+	    }
+	}
+
+    static void pattern_6(int n) {
+    //inner loop  
+    for (int i= 0; i<= n; i++)  
+    {  
+    //outer loop  
+    for (int j=1; j<=n-i; j++)  
+    {  
+    System.out.print(" ");  
+    }  
+    for (int k=0;k<=i;k++)  
+    {  
+    System.out.print("*");  
+    }   
+    System.out.println("");  
+    }  
+}
+}
+    
+
